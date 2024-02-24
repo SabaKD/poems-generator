b7318925d6b78c46bf97bd32daeb97b2ce86a774
SabaKD <saba.zadeh96@gmail.com>
2024-02-24 11:40:34 +0100

    added UX

--- src/index.js
+++ src/index.js
@@ -14,10 +14,13 @@ function generatePoem(event){
 
   let instructionInput = document.querySelector("#user-instructions");
   let apiKey = "4bc76te01aac743d12o764377f46072e";
-  let context = "you are a romantic poem generator and like to make short poems, your mission is to generate a 4 line (use a new line for each hemistich) poem in basic HTML";
+  let context = "you are a romantic poem generator and like to make short poems and very strict about the instructions, instruction:your mission is to generate a 4 line (use a new line for each hemistich) poem";
   let prompt =` generate a poem with the word ${instructionInput.value}`
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement =document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
     

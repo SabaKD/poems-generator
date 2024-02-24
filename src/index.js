76d12a6c72e41ae81c0f08c8bbd7a2cd62f84421
SabaKD <saba.zadeh96@gmail.com>
2024-02-24 11:00:14 +0100

    added AI api intergration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 
-function generatePoem(event){
-    event.preventDefault();
-    new Typewriter('#poem', {
-  strings: "Hello World  kwjfkjehfkejf",
+function displayPoem(response){
+  new Typewriter('#poem', {
+  strings: response.data.answer,
   autoStart: true,
-  delay: 30
+  delay: 30,
+  cursor: ""
 });
+}
+
+
+function generatePoem(event){
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#user-instructions");
+  let apiKey = "4bc76te01aac743d12o764377f46072e";
+  let context = "you are a romantic poem generator and like to make short poems, your mission is to generate a 4 line (use a new line for each hemistich) poem in basic HTML";
+  let prompt =` generate a poem with the word ${instructionInput.value}`
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+  axios.get(apiURL).then(displayPoem);
+    
 }
 
 let poemForm = document.querySelector("#poem-generator");
